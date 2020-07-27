9e404b1785164b9803f6a2bde971a99e71b63225
Season Shrestha <czon.herald75@gmail.com>
2020-07-27 09:25:19 +0545

    made content optional. removed about page

--- classroom/models.py
+++ classroom/models.py
@@ -9,7 +9,7 @@ from django.utils.text import slugify
 
 class Grade(models.Model):
     grade_title = models.CharField(max_length=100)
-    grade_description = models.TextField()
+    grade_description = models.TextField(blank=True)
 
     def __str__(self):
         return self.grade_title
@@ -17,7 +17,7 @@ class Grade(models.Model):
 
 class Module(models.Model):
     module_title = models.CharField(max_length=100)
-    module_description = models.TextField()
+    module_description = models.TextField(blank=True)
     module_author = models.ForeignKey(User, on_delete=models.CASCADE)
     grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
 
@@ -27,7 +27,7 @@ class Module(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     module = models.ForeignKey(Module, null=True, blank=True,
---
+++ media/documents/begin.py
@@ -0,0 +1,92 @@
+# print(r'C:\file\file2\file3')
+# print(3 * 'sea' + 'son')
+# print('py' 'thon')
+# word = 'season'
+# print(word[5])
+# print(word[-1])
+# print(word[-3:5])
+#
+# # name = input("Enter your name: ")
+# # print("Your name is ""which will be displayed here with slicing " + name[3:])
+# # print("yor name is " + 3 * name)
+# # print("done")
+#
+# # print('My name is '+word[:3]+'son')
+# # print(len(word))
+# # print('The sum of 1+2 is {}'.format(1+2))
+# # print(word.capitalize())
+# print(word.lower())
+# str = "this is string example....wow!!!"
+#
+# print(len(str))
+# winner = "Season"
+# print("The winner is {}".format(winner).center(30, '*'))
+# test = "i am repeated repeated twice "
+# sub = "repeated"
+# name = "Season Shresthaa"
+# print(name.endswith("Shrestha"))
+#
+# print(test.count(sub))
+
+# name = input("Enter your name: ")
+# test_name = name.lower()
+# rem_space = test_name.replace(" ", "")
+# substr = "s"
+# # if rem_space.endswith("shrestha"):
+#     print("Your name is {}".format(name.center(20, '*')) +
+#           " \n There are {} s and has total length of {} "
+#           .format(rem_space.count(substr), len(rem_space)))
+#     print("\n The s is in the index ", rem_space.find("s"))
+#     # print(test_name.center(20, '*'))
+# print("s" in name)
+# print(name.index("h"))
+# print(name.isalpha())
+# print(name.isalnum())
+# print(name.isspace())
+# print(name.isdecimal())
+# seq = ("season", "Shrestha", "Czon", "Shrestha")
+# join_by = "-"
+# joined = join_by.join(seq)
+# print(joined)
+# str = ("Season", "Czon", "Sezon")
+# if str.startswith("S"):
+#     print("Starting with S")
+# print(str.lstrip("sS"))
+# print(str.swapcase())
+# new_name = str.replace("Season", "Czon")
+# print(new_name)
+# str_new = "-"
+# print(str_new.join(str))
+# name = "S-e-a-s-o-n"
+# new_name = name.split("-")
+# print(type(new_name))
+# print(new_name)
+# name = "season shrestha"
+# print(name.title())
+# squares = [1, 4, 9, 16, 25]
+# odds = [1, 3, 7, 9]
+# print(squares+[9, 100, 200])
+# squares[3] = 100
+# squares.append(5 ** 2)
+# print(squares)
+# x = [squares, odds]
+# print(x[0][1])
+# x = ["list", "of", 1, 2, 3, 4]
+# print(len(x))
+# print(x[0])
+# if "of" in x:
+#     print("There is x")
+# print(x)
+# x = [123, 456]
+# y = [557, 678]
+#comment
+#
+# print(cmp(y, x))
+# x = ["Season", "Shrestha"]
+# y = ["czon", "Shrestha"]
+# print(cmp(x, y))
+# x = ['z']
+# y = ['x']
+# print(cmp(x, y))
+# print(ord('z'))
+# print(ord('x'))
